f5458ed01f1e88ba068cff4f5d117ed87f5c1918
KangDroid <hyunwoo286@gmail.com>
2021-04-28 21:51:27 +0900

    [MainActivity] Implement viewbinding on mainactivity

--- app/src/main/java/com/kangdroid/k_labapplication/MainActivity.kt
+++ app/src/main/java/com/kangdroid/k_labapplication/MainActivity.kt
@@ -2,10 +2,15 @@ package com.kangdroid.k_labapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.kangdroid.k_labapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private val activityMainBinding: ActivityMainBinding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(activityMainBinding.root)
     }
 }
