c225e3b14d07761350a4a9602472bab6d8679c5d
jiwoo <hjw6112@konkuk.ac.kr>
2021-06-02 15:10:47 +0900

    login2

--- app/src/main/java/com/kangdroid/k_labapplication/MainActivity.kt
+++ app/src/main/java/com/kangdroid/k_labapplication/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.kangdroid.k_labapplication
 
+import android.graphics.PorterDuff
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.content.ContextCompat
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 import com.kangdroid.k_labapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +16,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(activityMainBinding.root)
+        init()
+    }
+
+    fun init(){
     }
 }
